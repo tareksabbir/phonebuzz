6d1cf5e56944ad2cd6618698c7a929194291292e
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-02-28 23:57:38 +0600

    search-value

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('js is ok');
+const searchPhone = async () => {
+    const searchFieldInput = document.getElementById('srearch-field');
+    const searchField = searchFieldInput.value;
+    console.log(searchField);
+    searchFieldInput.value = '';
+    if (searchField == '') {
+        // error sms
+
+    } else {
+        // load data 
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`
+        const res = await fetch(url);
+        const data = await res.json();
+        displaySearchResult(data.status);
+
+    }
+}
+
+const displaySearchResult = data => {
+    console.log(data)
+}
