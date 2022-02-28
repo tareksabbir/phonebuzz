acad68c9d73a3c0e00c85d662a5ea2a0f5c332cb
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-03-01 01:23:20 +0600

    update

--- js/app.js
+++ js/app.js
@@ -11,11 +11,36 @@ const searchPhone = async () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchField}`
         const res = await fetch(url);
         const data = await res.json();
-        displaySearchResult(data.status);
+        displaySearchResult(data.data);
 
     }
 }
 
 const displaySearchResult = data => {
-    console.log(data)
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    data.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick ="loadMealDetail('${phone.slug}')" class="card h-100 p-3">
+              <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="">
+              <div class="card-body text-center">
+                   <h5 class="card-title">${phone.phone_name}</h5>
+                   <p class="card-text">${phone.brand}</p>
+             </div>
+             <button type="button" class="btn btn-primary w-40 mx-auto" style="border-radius: 1rem; background-color: rgb(56, 162, 247); padding: 6px 20px; border:none;">View Details</button>
+
+        </div>
+        `;
+        searchResult.appendChild(div);
+
+    });
+
 }
+
+
+
+
+
